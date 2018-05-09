02085de1b0178c0cc38a7cd0038263888469d49e
Gabriela Melo <gabimelo10@gmail.com>
2018-05-08 21:47:42 -0300

    i think its working

--- adult-pt3/clustering.R
+++ adult-pt3/clustering.R
@@ -13,10 +13,16 @@ df <- data.frame("age" = full_df$age,
                  "sex" = full_df$sex,
                  "hours.per.week" = full_df$hours.per.week)
 
+# JUST FOR TESTING
+df <- data.frame("age" = full_df$age, 
+                 "fnlwgt" = full_df$fnlwgt)
+
 # z score normalization
 
 z_score <- function(df){
   continuous_names <- c("age", "fnlwgt", "education.num", "hours.per.week")
+  # JUST FOR TESTING
+  continuous_names <- c("age", "fnlwgt")
   for (name in continuous_names){
     mf <- mean(df[[name]])
     sf <- sum(sapply(df[[name]], function(x) abs(x - mf)))/600
@@ -30,42 +36,57 @@ z_score <- function(df){
 df <- z_score(df)
 
 # distance function
-a_dist <- function(df) {
-  return(NULL)
+a_dist <- function(v1, v2){
+  sqrt(sum((v1-v2)^2))
 }
 
+# function dist_make of package usedist does this
 create_a_dist <- function (dataframe, custom_dist=a_dist) {
-  mat <- matrix(, nrow = nrow(dataframe), ncol = nrow(dataframe))
-  for (i in 1:nrow(dataframe)){
-    for (j in 1:nrow(dataframe)){
+  # JUST FOR TESTING
+  mat <- matrix(0, nrow=10, ncol=10)
+  for (i in 1:10){
+    for (j in 1:10){
+  #mat <- matrix(0, nrow = nrow(dataframe), ncol = nrow(dataframe))
+  #for (i in 1:nrow(dataframe)){
+  #  for (j in 1:nrow(dataframe)){
       mat[i,j] <- custom_dist(dataframe[i,], dataframe[j,])
     }
   }
   return(mat)
 }
 
-silhouette_coefficient <- function() {
-  
-  # s(i)!=!(b(i)!–!a(i))!/!max{a(i),b(i)}!
-  # bad!!!Y1!≤!s(i)!≤!1!!good
-  # where i is each datum, 
-  # a(i) is the average distance between i and all other data within same cluster, 
-  # and b(i) is lowest average distance of i to all points in any other cluster
-  
-  return(NULL)
-}
+create_a_dist(df)[0:10,0:10]
 
 # clustering
+library(flexclust)
 set.seed(42)
 
-k = 2
-k = 4
-k = 6
-
-# perhaps check out this package
-# https://cran.r-project.org/web/packages/flexclust/index.html
-custom_kmeans <- function() {
+# get structure of dist function for kccaFamily from distEuclidean
+create_a_dist_for_centers <- function(dataframe, centers) {
+  if (ncol(dataframe) != ncol(centers)) 
+    stop(sQuote("x"), " and ", sQuote("centers"), " must have the same number of columns")
   
+  mat <- matrix(0, nrow = nrow(dataframe), ncol = nrow(centers))
+  for (i in 1:nrow(dataframe)){
+    for (j in 1:nrow(centers)){
+      mat[i,j] <- a_dist(dataframe[i,], centers[j,])
+    }
+  }
+  return(mat)
 }
 
-cluster <- custom_kmeans(df, k)
+k_values = c(2,4,6)
+
+for (k in k_values) {
+  model <- kcca(df,k,family=kccaFamily(dist=create_a_dist_for_centers, cent=colMeans))
+  print('silhoutte for k:')
+  print(k)
+  print(Silhouette(model, df))
+}
+
+# to see the clusters
+#image(model)
+# to see the final centroid positions 
+#parameters(model)
+# to see clusters to which original data were assigned to
+#clusters(model)
--- adult-pt3/testing.R
+++ adult-pt3/testing.R
@@ -1,89 +1,39 @@
-df = data.frame(c(1,2,3,4), c(2,3,4,5), c(4,5,6,7))
-names(df) <- c("c1","c2","c3")
+old_vals <- c(1,2,1.3,2.3,1.6,2.6,6,7,6.3,7.3,6.6,7.6)
+vals <- c(1,2,1.3,2.3,4.6,2.6,6,7,6.3,7.3,6.6,7.6)
+df = data.frame(old_vals,vals)
+names(df) <- c("c1","c2")
+#plot(df)
 
 custom_dist <- function(v1, v2){
   sqrt(sum((v1-v2)**2))
 }
 
-dist_make(df, custom_dist)
+for (k in 1:nrow(centers)) {
+  z[, k] <- sqrt(colSums((t(x) - centers[k, ])^2))
+}
 
-a_dist_make <- function(a_df, a_custom_dist) {
-  mat <- matrix(, nrow = nrow(a_df), ncol = nrow(a_df))
-  for (i in 1:nrow(a_df)){
-    for (j in 1:nrow(a_df)){
-      mat[i,j] <- a_custom_dist(df[i,], df[j,])
+sqrt(colSums((t(x) - centers[k, ])^2))
+
+
+a_dist_make <- function(dataframe, centers) {
+  #print('no')
+  #print(centers)
+  #print(df)
+  
+  if (ncol(dataframe) != ncol(centers)) 
+    stop(sQuote("x"), " and ", sQuote("centers"), " must have the same number of columns")
+  
+  mat <- matrix(0, nrow = nrow(dataframe), ncol = nrow(centers))
+  for (i in 1:nrow(dataframe)){
+    for (j in 1:nrow(centers)){
+      mat[i,j] <- custom_dist(dataframe[i,], centers[j,])
     }
   }
+  #print(mat)
   return(mat)
 }
 
-a_dist_make(df, custom_dist)
 
-dist(df)
-
-#trying many different things
-df = data.frame(c(1,2,3,4), c(2,3,4,5), c(4,5,6,7))
-names(df) <- c("c1","c2","c3")
-
-old_vals <- c(1,2,1.3,2.3,1.6,2.6,6,7,6.3,7.3,6.6,7.6)
-vals <- c(1,2,1.3,2.3,4.6,2.6,6,7,6.3,7.3,6.6,7.6)
-df = data.frame(vals,vals2)
-names(df) <- c("c1","c2")
-
-custom_dist <- function(v1, v2){
-  sqrt(sum((v1-v2)**2))
-}
-
-#dist_make(df, custom_dist)
-
-a_dist_make <- function(a_df, centers=NULL) {
-  a_custom_dist <- function(v1, v2){
-    #print(v1)
-    sqrt(sum((v1-v2)**2))
-  }
-  if(is.null(centers)){
-    print('yes')
-    
-    mat <- matrix(, nrow = nrow(a_df), ncol = nrow(a_df))
-    for (i in 1:nrow(a_df)){
-      for (j in 1:nrow(a_df)){
-        mat[i,j] <- a_custom_dist(df[i,], df[j,])
-      }
-    }
-    #print(as.dist(mat))
-    return(as.dist(mat))
-    #return(mat)
-  }
-  else {
-    print('no')
-    print(centers)
-    print(df)
-    
-    if (ncol(a_df) != ncol(centers)) 
-      stop(sQuote("x"), " and ", sQuote("centers"), " must have the same number of columns")
-    
-    mat <- matrix(, nrow = nrow(a_df), ncol = nrow(centers))
-    for (i in 1:nrow(a_df)){
-      for (j in 1:nrow(centers)){
-        mat[i,j] <- a_custom_dist(df, centers[j,])
-      }
-    }
-    print(mat)
-    return(mat)
-  }
-}
-
-a_dist_make(df)
-
-dist(df)
-
-image(kcca(df,2))
-# to see the final centroid positions 
-parameters(kcca(df,2))
-# to see clusters to which original data were assigned to
-clusters(kcca(df,2))
-
-kcca(df,2,family=kccaFamily(dist=a_dist_make))
 kcca(df,2,family=kccaFamily(dist=a_dist_make,cent="mean"))
 # kcca(df,2,family=kccaFamily(dist=a_custom_dist,cent="mean"))
 
@@ -92,18 +42,6 @@ distEuclidean
 
 
 # dont even know anymor
-df = data.frame(c(1,2,3,4), c(2,3,4,5), c(4,5,6,7))
-names(df) <- c("c1","c2","c3")
-
-old_vals <- c(1,2,1.3,2.3,1.6,2.6,6,7,6.3,7.3,6.6,7.6)
-vals <- c(1,2,1.3,2.3,4.6,2.6,6,7,6.3,7.3,6.6,7.6)
-df = data.frame(vals,vals2)
-names(df) <- c("c1","c2")
-
-custom_dist <- function(v1, v2){
-  sqrt(sum((v1-v2)**2))
-}
-
 a_dist_make <- function(a_df, centers=NULL) {
   if(is.null(centers)){
     print('yes')
@@ -140,36 +78,3 @@ a_dist_make <- function(a_df, centers=NULL) {
   }
 }
 
-a_dist_make(df)
-
-dist(df)
-
-library(flexclust)
-
-image(kcca(df,2))
-# to see the final centroid positions 
-parameters(kcca(df,2))
-# to see clusters to which original data were assigned to
-clusters(kcca(df,2))
-
-
-my_eucl <- function (x, centers) {
-  print(x)
-  print(centers)
-  if (ncol(x) != ncol(centers)) 
-    stop(sQuote("x"), " and ", sQuote("centers"), " must have the same number of columns")
-  z <- matrix(0, nrow = nrow(x), ncol = nrow(centers))
-  for (k in 1:nrow(centers)) {
-    z[, k] <- sqrt(colSums((t(x) - centers[k, ])^2))
-  }
-  print(z)
-  z
-}
-
-kcca(df,2,family=kccaFamily(dist=a_dist_make))
-kcca(df,2,family=kccaFamily(dist=my_eucl))
-kcca(df,2,family=kccaFamily(dist=distEuclidean))
-kcca(df,2,family=kccaFamily(dist=a_dist_make,cent="mean"))
-# kcca(df,2,family=kccaFamily(dist=a_custom_dist,cent="mean"))
-
-distEuclidean
